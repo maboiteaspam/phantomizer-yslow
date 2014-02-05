f6c18848c8ea64eb415dea4bc48774e8c2f85c22
clement <maboiteaspam@wanadoo.fr>
2014-02-05 22:15:08 +0800

    Better grunt options for test suite

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,94 +4,94 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     yslow: {
+      options:{
+        readable:true,
+        output:"yslow_reports"
+      },
 
       test_json_1_file: {
         options: {
-          urls:['http://localhost:8080/index.html'],
-          output:"yslow_reports"
+          urls:['http://localhost:8080/index.html']
         }
       },
       test_json_3_files: {
         options: {
-          urls:['http://localhost:8080/index.html','http://localhost:8080/index2.html','http://localhost:8080/index3.html'],
-          output:"yslow_reports"
+          urls:['http://localhost:8080/index.html','http://localhost:8080/index2.html','http://localhost:8080/index3.html']
         }
       },
       test_json_duplicated_file: {
         options: {
-          urls:['http://localhost:8080/index.html','http://localhost:8080/index2.html','http://localhost:8080/index2.html'],
-          output:"yslow_reports"
+          urls:['http://localhost:8080/index.html','http://localhost:8080/index2.html','http://localhost:8080/index2.html']
         }
       },
       test_file_path: {
         options: {
-          urls:['http://localhost:8080/index.html','http://localhost:8080/some/index.html'],
-          output:"yslow_reports"
+          urls:['http://localhost:8080/index.html','http://localhost:8080/some/index.html']
         }
       },
 
       test_json: {
         options: {
-          urls:['http://localhost:8080/index.html','http://localhost:8080/index2.html']
+          urls:['http://localhost:8080/index.html','http://localhost:8080/index2.html'],
+          output:null
         }
       },
       test_xml: {
         options: {
           urls:['http://localhost:8080/index.html','http://localhost:8080/index2.html'],
-          format:"xml"
+          format:"xml",
+          output:null
         }
       },
       test_plain: {
         options: {
           urls:['http://localhost:8080/index.html','http://localhost:8080/index2.html'],
-          format:"plain"
+          format:"plain",
+          output:null
         }
       },
       test_tap: {
         options: {
           urls:['http://localhost:8080/index.html','http://localhost:8080/index2.html'],
-          format:"tap"
+          format:"tap",
+          output:null
         }
       },
       test_junit: {
         options: {
           urls:['http://localhost:8080/index.html','http://localhost:8080/index2.html'],
-          format:"junit"
+          format:"junit",
+          output:null
         }
       },
 
       test_json_file: {
         options: {
-          urls:['http://localhost:8080/index.html','http://localhost:8080/index2.html'],
-          output:"yslow_reports"
+          urls:['http://localhost:8080/index.html','http://localhost:8080/index2.html']
         }
       },
       test_xml_file: {
         options: {
           urls:['http://localhost:8080/index.html','http://localhost:8080/index2.html'],
-          format:"xml",
-          output:"yslow_reports"
+          format:"xml"
         }
       },
       test_plain_file: {
         options: {
           urls:['http://localhost:8080/index.html','http://localhost:8080/index2.html'],
-          format:"plain",
-          output:"yslow_reports"
+          format:"plain"
         }
       },
       test_tap_file: {
         options: {
           urls:['http://localhost:8080/index.html','http://localhost:8080/index2.html'],
-          format:"tap",
-          output:"yslow_reports"
+          format:"tap"
         }
       },
       test_junit_file: {
         options: {
           urls:['http://localhost:8080/index.html','http://localhost:8080/index2.html'],
-          format:"junit",
-          output:"yslow_reports"
+          format:"junit"
         }
       }
 
